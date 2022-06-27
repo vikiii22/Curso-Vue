f30e0c9859f833052f26c89dae9c25bfa20adc25
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-06-27 11:06:02 +0200

    primera

---
+++ Proyecto-prueba-curso/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <h2>Amigos</h2>
+    <ul>
+        <li v-for="amigo in amigos" v-bind:key="amigo.id">{{ amigo.name }}</li>
+    </ul>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            amigos: [
+                {
+                    id:1,
+                    name:"Pepe",
+                    telefono:666112233,
+                    email:"pepepepito@correo.es"
+                },
+                {
+                    id:2,
+                    name:"Julia",
+                    telefono:666112233,
+                    email:"julia@correo.es"
+                }
+            ]
+        }
+    },
+
+    methods: {
+        
+    },
+}
+</script>
--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Jose manda"/>
+  <HelloWorld :msg="Jose"></HelloWorld>
 </template>
 
 <script>
