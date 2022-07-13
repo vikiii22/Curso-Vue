b57675631213405f2f41b2b346fba38988725ee5
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-07-13 13:38:11 +0200

    firebase

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="load">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,10 +21,42 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: [],
+    };
+  },
+
+  methods: {
+    load() {
+      fetch(
+        'https://vue-http-demo-jose-default-rtdb.europe-west1.firebasedatabase.app/hola.json'
+      )
+        .then((r) => {
+          if (r.ok) {
+            return r.json();
+          }
+        })
+        .then((data) => {
+          const result = [];
+          for (let id in data) {
+            result.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results=result;
+        });
+    },
+  },
+  mounted(){
+    this.load();
+  }
 };
 </script>
 
