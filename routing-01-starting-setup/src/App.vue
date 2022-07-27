ddffcd8ba7267be348b865227dd168b811100462
vikiii22 <joseviki10@gmail.com>
2022-07-27 08:00:28 +0200

    prueba

--- routing-01-starting-setup/src/App.vue
+++ routing-01-starting-setup/src/App.vue
@@ -20,7 +20,7 @@ export default {
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2', 'u6'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
-        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5', 'u7'] },
       ],
       users: [
         { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
@@ -29,6 +29,7 @@ export default {
         { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
         { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
         { id: 'u6', fullName: 'Jose Sánchez', role: 'Developer' },
+        { id: 'u7', fullName: 'Jose Sánchez', role: 'Developer' },
       ],
     };
   },
